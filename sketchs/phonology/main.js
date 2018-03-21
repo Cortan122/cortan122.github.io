614f53659204ac31411382bc31dc3c111129700c
Cortan122 <Cortan122@gmail.com>
2018-03-21 15:35:15 +0300

    new script type (phonology)

--- sketchs/phonology/main.js
+++ sketchs/phonology/main.js
@@ -130,7 +130,7 @@ var writer = function(){
   a.consonants = [
     'm','n','j',
     'p','t','k',
-    'f','s','l'
+    'f','s','l','_'
   ];
   a.vowels = ['a','e','i','o','u'];
   a.initOffset = [50,50];
@@ -144,7 +144,7 @@ var writer = function(){
   };
   a.write = function(data){
     var t = converter.syllables(data);
-    t = t.toLocaleLowerCase();
+    //t = t.toLocaleLowerCase();
     var arr = t.split(' ');
     for (var i = 0; i < arr.length; i++) {
       arr[i].match(/.?.?./g).forEach(a.writeSyllable);
@@ -163,8 +163,22 @@ var writer = function(){
     text(t,a.initOffset[0],height-a.initOffset[1]);
     if(t != '')a.write(data);
   };
+  a.writeSpace = function(){
+    push();
+    fill(0);
+    rect(a.curserx,a.cursery,a.curserx+a.size,a.cursery+a.size);
+    pop();
+    a.advance();
+  };
+  a.advance = function() {
+    a.curserx += a.size+a.gap;
+    if(a.curserx > width-a.initOffset[0]*1.5){
+      a.curserx = a.initOffset[0];
+      a.cursery += a.size+a.gap;
+    }
+  };
   a.writeSyllable = function(data){
-    if(data[0] != '_')a.writeConsonant(data[0]);
+    if(data[0] != '_')a.writeConsonant(data[0].toLocaleLowerCase());
     if(data[1] != '_')a.writeVowel(data[1]);
     if(data[2] != '_')a.writeNasal(data[2]);
     a.advance();
@@ -194,26 +208,74 @@ var writer = function(){
   a.writeNasal = function(data){
     point(a.curserx+a.size/4*3,a.cursery+a.size/4);
     ellipse(a.curserx+a.size/4*3+.5,a.cursery+a.size/4+.5,a.size/2,a.size/2);
-  }
-  a.writeSpace = function(){
-    push();
-    fill(0);
-    rect(a.curserx,a.cursery,a.curserx+a.size,a.cursery+a.size);
-    pop();
-    a.advance();
-  };
-  a.advance = function() {
-    a.curserx += a.size+a.gap;
-    if(a.curserx > width-a.initOffset[0]*2){
-      a.curserx = a.initOffset[0];
-      a.cursery += a.size+a.gap;
-    }
   };
   return a;
 }();
 //</writer>
 
+//<writer2>
+var consonantGraphics = [
+  'ln(0,0,1,0);ln(0,0,1,1);',//'m'
+  'ellipse(0.5,0.5,1,1);ln(0,0,0,1);',//'n'
+  'ln(0,0,1,0);ln(0,1,1,0);',//'j'
+  'ln(0,0,1,1);ln(0.5,0.5,0,1);',//'p'
+  'ln(0,1,.5,0);ln(1,1,.5,0);',//'t'
+  'ln(0,1,1,0);ln(0.5,0.5,1,1);',//'k'
+  'ln(0,1,1,1);ln(0,1,1,0);',//'f'
+  'ln(0,0,.5,1);ln(1,0,.5,1);',//'s'
+  'ln(0,1,1,1);ln(0,0,1,1);',//'l'
+  'ellipse(0.5,0.5,1,1);'//'_'
+];
+
+var vowelGraphics = [
+  '',//a 
+  ['ln(.5,.5,.5,0)','ln(.5,.25,.5,.75)','$0','$0','ln(.5,0,.5,.75)','$0','ln(.5,.5,.5,1)','ln(.5,.25,.5,1)','$6'],//e
+  ['pnt(.66,.33)','pnt(.5,.5)','pnt(.33,.33)','$0','$1','$2','pnt(.66,.66)','$1','pnt(.33,.66)'],//i
+  'ln(0,.5,1,.5)',//o
+  ['ln(.5,.5,1,.5)','ln(.25,.5,.75,.5)','ln(.5,.5,0,.5)','$0','$1','$2','$0','$1','$2']//u
+];
+
+writer.writeSpace = writer.advance;
+
+writer.writeSyllable = function(data){
+  var a = writer;
+  push();
+  var scl = 2/a.size;
+  var pnt = (x,y)=>ellipse(x,y,scl,scl);
+  var ln = line;
+  strokeWeight(scl);
+  translate(a.curserx,a.cursery);
+  scale(a.size);
+  var cind = a.consonants.indexOf(data[0].toLocaleLowerCase());
+  var cmd = consonantGraphics[cind];
+  eval(cmd);
+  if(data[0].match(/[A-Z]/)){
+    line(0,0,1,0);
+  }
+  if(data[2] != '_')line(1,0,1,1);
+  var vind = a.vowels.indexOf(data[1].toLocaleLowerCase());
+  cmd = vowelGraphics[vind];
+  if(typeof cmd == 'string'){
+    eval(cmd);
+  }else{
+    if(cind == 9)cind = 1;
+    var str = cmd[cind];
+    if(str[0] == '$'){
+      str = cmd[parseInt(str[1])];
+    }
+    eval(str);
+  }
+  pop();
+  a.advance();
+}
+//</writer2>
+
 var loremispum = 'nottoPiFiponaliotkuTaFoSnikajetetopLeFLatnojepLeTstaFlenijeluTejpoLitajusjiknaslaSTenijeiFoskFalajusjikstLaTanijaaLaskLojupeLeTFamiFsukaLtinuiLaSjasnuttoimennoKoFoLiletottjeloFekotkLiFsijistinukotoLoKoaPinaSFalSoTtjimstastliFojSiSniTejstFitelnoniktoneotFeLKajetnepLeSiLajetneiSPeKajetnaslaSTenijtolkoiSSatoKottoetonaslaSTenijanolisiSSatoKottotekktoneumejetLaSumnopLeTaFatsanaslaSTenijampostiKajutFelikijestLaTanijaLaFnokaknetnikoKoktoFoSluPilPipLeTpotjeliFoSSaSTalPisamostLaTanijetolkoSatottoetostLaTanijeanepotomuttoinojLaSFoSnikajuttakijeoPstojatelstFakoKTastLaTanijaiPolpLinosatnekojeinemalojenaslaSTenijeesliFospolSoFatsapLostejsimpLimeLomtoktoiSnasstalPiSanimatsakakimiPitoniPilotaKostnimifiSitjeskimiupLaSnenijamiesliPietonepLinosilossoPojnekojejpolSiiktomoKPipospLaFeTliFostiupLeknutstLemasjeKosaknaslaSTenijukotoLojenenesloPissoPojnikakiknepLijatnostejilitoKoktoiSPeKalPitakoKostLaTanijakotoLojenepLinosiloPissoPojnikakoKonaslaSTenijanomipoLitajemistjitajemPeSusloFnoSasluSiFajusjimispLaFeTliFoKoneKoToFanijatekktoPuTutjioPolsjennimiLaSFLasjennimsoPlaSnamipLeTstaFlajusjiksaimnaslaSTenijFisstuplenijistLastinepLeTFiTatkakijestLaTanijaikakijenestastjaikoSiTajutoniFinoFnitakSekakitektopoTuseFnojslaPostitoestiSSelanijaiSPeSatstLaTanijiPoliotkaSiFajetsaotispolnenijasFojeKoTolKaFpLotjemSTesotjenleKkoipLostopLoFestiLaSlitjijapotomuttokoKTamisFoPoTniinampLeTostaFlenapolnajaFoSmoSnostFiPoLaSelajemoKokoKTanittonemesajetnamTelattottonamPolsenLaFitsaluPojenaslaSTenijesleTujetpLiSnatSelannimaluPojestLaTanijeotFLatitelnimnopLinekotoLikoPstojatelstFakilipotLePoFanijuTolKailiFsilukakojtoneoPkoTimostitastopLikoTitsaSaPiFatonaslaSTenijakinePeSattaKostejpoetomumuTLetpLiTeLSiFajetsaFetomslutajesleTujusjeKopLintipaFiPoLailiotkaSiFajasotuToFolstFijaonpolutajetkakijetoinijeiTaSePolsijenaslaSTenijailipLeteLpeFajastLaTanijaoniSPaFlajetsaotPolejeSestokik';
+
+var alphabet = 'maajapatakafasalameejepetekefeselemiijipitikifisilimoojopotokofosolomuujuputukufusulu';
+var alphabetaligned = 'maajapatakafasala aaaaaaa meejepetekefesele aaaaaaa miijipitikifisili aaaaaaa moojopotokofosolo aaaaaaa muujuputukufusulu aaaaaaa';
+var abjad = 'maajapatakafasala';
+var abjadaligned = 'maaja aaaaaaaaaaaaa pataka aaaaaaaaaaaaa fasala aaaaaaaaaaaaa ';
 
 var syllables = converter.syllables;
 

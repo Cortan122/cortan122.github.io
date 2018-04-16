ede79ec6eb1aa139e4dcff109e308dd50cbb1125
Cortan122 <Cortan122@gmail.com>
2018-04-16 22:12:07 +0300

    attempt at an input manager (etc)

--- etc/C/tests/input.cpp
+++ etc/C/tests/input.cpp
@@ -1,54 +1,43 @@
+/*
+  Linux (POSIX) implementation of _kbhit().
+  Morgan McGuire, morgan@cs.brown.edu
+*/
 #include <stdio.h>
-#include <stdlib.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <unistd.h>
-#include <fcntl.h>
+#include <sys/ioctl.h>
+#include <sys/select.h>
 #include <termios.h>
+#include <stropts.h>
 
-int inputHandler(char * c){
-  printf(":%d,%d,%d:\n",c[0],c[1],c[2]);
-  if(c[0] == 'q')return 1;
-  return 0;
-}
+int _kbhit() {
+  static const int STDIN = 0;
+  static bool initialized = false;
 
-void initInputHandler(){
-  struct termios oldSettings, newSettings;
-
-  tcgetattr( fileno( stdin ), &oldSettings );
-  newSettings = oldSettings;
-  newSettings.c_lflag &= (~ICANON & ~ECHO);
-  tcsetattr( fileno( stdin ), TCSANOW, &newSettings );
-  char * c = (char *)malloc(10);    
-
-  while ( 1 ){
-    fd_set set;
-    struct timeval tv;
-
-    tv.tv_sec = 10;
-    tv.tv_usec = 0;
-
-    FD_ZERO( &set );
-    FD_SET( fileno( stdin ), &set );
-
-    int res = select( fileno( stdin )+1, &set, NULL, NULL, &tv );
-
-    if( res > 0 ){
-      //printf( "Input available\n" );
-      read( fileno( stdin ), c, 3 );
-      if(inputHandler(c))break;
-      //printf(":%d,%d,%d:\n",c[0],c[1],c[2]);
-      //printf(":%c:\n",c);
-    }else if( res < 0 ){
-      perror( "select error" );
-      break;
-    }
+  if (!initialized) {
+    // Use termios to turn off line buffering
+    termios term;
+    tcgetattr(STDIN, &term);
+    term.c_lflag &= ~ICANON;
+    tcsetattr(STDIN, TCSANOW, &term);
+    setbuf(stdin, NULL);
+    initialized = true;
   }
 
-  tcsetattr( fileno( stdin ), TCSANOW, &oldSettings );
+  int bytesWaiting;
+  ioctl(STDIN, FIONREAD, &bytesWaiting);
+  return bytesWaiting;
 }
 
-int main(){
-  initInputHandler();
+#include <unistd.h>
+
+const int time = 1000;
+
+int main(int argc, char** argv) {
+  while(1){
+    while(!_kbhit()){
+      usleep(time);
+    }
+    printf("%c\n",getchar());
+    fflush(stdout);
+  }
   return 0;
-}
+} 

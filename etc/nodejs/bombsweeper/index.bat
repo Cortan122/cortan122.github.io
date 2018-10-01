56bffcabeda9ad4589632d98fc8314ec2ded49db
Cortan122 <Cortan122@gmail.com>
2018-10-01 23:59:14 +0300

    renamed favicon (nodejs/bombsweeper)

--- etc/nodejs/bombsweeper/index.bat
+++ etc/nodejs/bombsweeper/index.bat
@@ -4,7 +4,7 @@ echo sLinkFile = "%~dp0\..\Bombsweeper.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
 echo oLink.TargetPath = "%~dp0\run.bat" >> CreateShortcut.vbs
 echo oLink.Description  = "A minesweeper clone" >> CreateShortcut.vbs
-echo oLink.IconLocation = "%~dp0\mine.ico" >> CreateShortcut.vbs
+echo oLink.IconLocation = "%~dp0\favicon.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
 cscript CreateShortcut.vbs
 del CreateShortcut.vbs
